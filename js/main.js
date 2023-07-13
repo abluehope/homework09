5342c49cd0d2385188bd1c6ebc675c9d7d471cb9
abluehope <100765576+abluehope@users.noreply.github.com>
2023-07-13 18:09:31 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(function () {
+  $(window).on("scroll", function () {
+    let sct = $(window).scrollTop();
+    if (sct > 0) {
+      $(".header, .to_top").addClass("on");
+    } else {
+      $(".header, .to_top").removeClass("on");
+    }
+  });
+
   $(".custom_service .la_chg span").on("click", function () {
     $(this).toggleClass("on");
     $(this).next().toggleClass("on");
@@ -18,5 +27,51 @@ $(function () {
   });
   $(".main_visual .arrows .right").on("click", function () {
     $(".main_slide").slick("slickNext");
+  });
+
+  // 공지사항 대관공고 탭
+  $(".tab_link li").on("click", function (event) {
+    event.preventDefault();
+
+    let idx = $(this).index();
+
+    $(this).addClass("on").siblings().removeClass("on");
+
+    $(".tab_content .con").eq(idx).addClass("on").siblings().removeClass("on");
+  });
+
+  $(".banner_slide").slick({
+    arrows: false,
+  });
+
+  $(".main_notice .banner_arrows .left").on("click", function () {
+    $(".banner_slide").slick("slickPrev");
+  });
+  $(".main_notice .banner_arrows .right").on("click", function () {
+    $(".banner_slide").slick("slickNext");
+  });
+
+  $(".movie_slide").slick({
+    arrows: false,
+    variableWidth: true,
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  $(".main_movie .arrow_next .next").on("click", function () {
+    $(".movie_slide").slick("slickNext");
   });
 });
